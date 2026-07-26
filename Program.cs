c459e803dba74fd06ae9c0a973393074ce1a76a8
Myles Marcus <152407286+MylesMarcus102@users.noreply.github.com>
2026-07-25 21:32:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+UserPokemon User = new UserPokemon();
+
+public class Pokemon
+{ 
+    public int health;
+    public int speed;
+    public int damage;
+    public string type;
+    public int defense;
+    public string choice;
+}
+
+public class UserPokemon : Pokemon
+{
+    public void SelectionTypes()
+    {
+        while (true)
+        {
+            Console.WriteLine("Please select between the following pokemon: Charmander, pikachu, squirtle, or bulbasaur.");
+            choice = Console.ReadLine().ToLower();
+            if (choice == "charmander")
+            {
+
+                break;
+            }
+            else if (choice == "pikachu")
+            {
+
+                break;
+            }
+            else if (choice == "squirtle")
+            {
+                
+                break;
+            }
+            else if (choice == "bulbasaur")
+            {
+
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please retype your response as written below. It is not sensitive to capitilazation.");
+            }
+        }
+    }
+}
+
+public class OpponentPokemon : Pokemon
+{
+    
+}
+
+
