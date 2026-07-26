5f6225d2ad289208637dedeada482a26646ecf85
Myles Marcus <152407286+MylesMarcus102@users.noreply.github.com>
2026-07-26 11:26:39 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.ComponentModel.Design;
 
-UserPokemon User = new UserPokemon();
+public enum PokemonType
+{
+    Fire,
+    Water,
+    Electric,
+    Grass
+}
 
 public class Pokemon
 { 
     public int health;
     public int speed;
-    public int damage;
-    public string type;
+    public int attack;
+    public PokemonType type;
     public int defense;
     public string choice;
 }
@@ -23,27 +28,43 @@ public class UserPokemon : Pokemon
             choice = Console.ReadLine().ToLower();
             if (choice == "charmander")
             {
-
+                health = 41;
+                attack = 24;
+                defense = 21;
+                speed = 28;
+                type = PokemonType.Fire;
                 break;
             }
             else if (choice == "pikachu")
             {
-
+                health = 41;
+                attack = 22;
+                defense = 17;
+                speed = 39;
+                type = PokemonType.Electric;
                 break;
             }
             else if (choice == "squirtle")
             {
-                
+                health = 46;
+                attack = 24;
+                defense = 28;
+                speed = 22;
+                type = PokemonType.Water;
                 break;
             }
             else if (choice == "bulbasaur")
             {
-
+                health = 47;
+                attack = 24;
+                defense = 24;
+                speed = 22;
+                type = PokemonType.Grass;
                 break;
             }
             else
             {
-                Console.WriteLine("Please retype your response as written below. It is not sensitive to capitilazation.");
+                Console.WriteLine("Please retype your response as written below.");
             }
         }
     }
